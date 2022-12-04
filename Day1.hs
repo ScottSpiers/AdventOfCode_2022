9525d603a87c5885ffe4b8f34e323f0cd3df2b7c
ScottSpiers <scott.m.spiers@gmail.com>
2022-12-04 21:28:46 +0000

    Initial Part 2

--- Day1.hs
+++ Day1.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import System.IO
+import Data.List
+import Data.Ord
 
-main = withFile' "day1.txt" day1
+main = withFile' "day1.txt" part2
 
 
 withFile' :: String -> (IO String -> IO ()) -> IO ()
@@ -12,10 +14,17 @@ withFile' path f = do
     hClose handle
     return result
 
-day1 :: IO String -> IO ()
-day1 contents = do
+part1 :: IO String -> IO ()
+part1 contents = do
     c <- contents
     print . maximum . map (sum) . (map . map) (read :: String -> Int) $ split $ lines c
+
+part2 :: IO String -> IO ()
+part2 contents = do
+    c <- contents
+    let x = map (sum) . (map . map) (read :: String -> Int) $ split $ lines c
+        in print $ sum $ take 3 (sortBy (comparing Down) x)
+
 
 split :: [String] -> [[String]]
 split s = case dropWhile (== "") s of
