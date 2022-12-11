5b88799fd69fdf42338b7856c0f060973599ed7d
ScottSpiers <scott.m.spiers@gmail.com>
2022-12-11 17:53:59 +0000

    Day 2 Implementation

--- Day1.hs
+++ Day1.hs
@@ -5,8 +5,10 @@ import Data.List
 import Data.Ord
 
 main = do 
-    part1
-    part2
+    dayOnePartOne
+    dayOnePartTwo
+    dayTwoPartOne
+    dayTwoPartTwo
 
 withFile' :: (Show a) => String -> (String -> a) -> IO ()
 withFile' path f = do
@@ -31,15 +33,78 @@ elvesTotalCalories s = map (sum) . mapRead . split $ lines s
 maxCalories :: String -> Int
 maxCalories s = maximum $ elvesTotalCalories s
 
-part1 = withFile' "day1.txt" maxCalories
+dayOnePartOne = withFile' "day1.txt" maxCalories
 
 top3Calories :: String -> Int
 top3Calories s = (sum . take 3 . sortBy (comparing Down)) $ elvesTotalCalories s
 
-part2 = withFile' "day1.txt" top3Calories
+dayOnePartTwo = withFile' "day1.txt" top3Calories
 
 
 -- Day 2 --
+
+data Choice = Rock | Paper | Scissors deriving (Bounded, Enum, Eq, Show)
+
+data Result = Win | Draw | Lose deriving (Enum, Show)
+
+
+pairUp :: [a] -> [(a, a)]
+pairUp [] = []
+pairUp (x:[]) = []
+pairUp (x:y:xs) = (x, y) : pairUp xs
+
+evaluate :: (Choice, Choice) -> Int
+evaluate (x, y) -- +1 as Enums start at 0
+    | x == y = fromEnum y + 4
+    | x == prev y = fromEnum y + 7
+    | otherwise = fromEnum y + 1
+
+next :: (Enum a, Bounded a, Eq a) => a -> a
+next x 
+        | x == maxBound = minBound
+        | otherwise = succ x
+
+prev :: (Enum a, Bounded a, Eq a) => a -> a
+prev x 
+        | x == minBound = maxBound
+        | otherwise = pred x
+
+assumedStrategyGuide :: String -> [(Choice, Choice)]
+assumedStrategyGuide s = pairUp . map (decodeChoice) $ words s
+
+calcStrategy :: (Enum a, Enum b) => ((a, b) -> Int) -> [(a, b)] -> Int
+calcStrategy f xs = foldl (\acc x -> acc + f x) 0 xs
+
+decodeChoice :: String -> Choice
+decodeChoice s -- keep x, y, z around so part 1 still works
+        | s == "A" || s == "X" = Rock
+        | s == "B" || s == "Y" = Paper
+        | s == "C" || s == "Z" = Scissors
+
+dayTwoPartOne = withFile' "day2.txt" (calcStrategy (evaluate) . assumedStrategyGuide)
+
+decodeResult :: String -> Result
+decodeResult s
+        | s == "X" = Lose
+        | s == "Y" = Draw
+        | s == "Z" = Win
+
+decode :: (String, String) -> (Choice, Result)
+decode (x, y) = (decodeChoice x, decodeResult y)
+
+evaulate' :: (Choice, Result) -> Int
+evaulate' (c, r) = case r of -- +1 as Enums start at 0
+            Win -> (fromEnum $ next c) + 7
+            Draw -> fromEnum c + 4
+            Lose -> (fromEnum $ prev c) + 1
+
+strategyGuide :: String -> [(Choice, Result)]
+strategyGuide s = map (decode) . pairUp $ words s
+
+dayTwoPartTwo = withFile' "day2.txt" (calcStrategy (evaulate') . strategyGuide)
+
+-- Day 3 --
+
 
 
 
