ec3149a24c1adf09bb68f3900d0a6da4439f78bc
ScottSpiers <scott.m.spiers@gmail.com>
2022-12-11 14:18:14 +0000

    Final day 1 cleanup

--- Day1.hs
+++ Day1.hs
@@ -8,33 +8,36 @@ main = do
     part1
     part2
 
-withFile' :: String -> (String -> IO ()) -> IO ()
+withFile' :: (Show a) => String -> (String -> a) -> IO ()
 withFile' path f = do
     handle <- openFile path ReadMode
     contents <- hGetContents handle
-    result <- f contents
+    print $ f contents
     hClose handle
-    return result
-
--- Day 1 --
-elvesTotalCalories :: String -> [Int]
-elvesTotalCalories s = map (sum) . (map . map) (read :: String -> Int) $ split $ lines s
-
-maxCalories :: String -> IO ()
-maxCalories s = print $ maximum $ elvesTotalCalories s
-
-part1 = withFile' "day1.txt" maxCalories
-
-top3Calories :: String -> IO ()
-top3Calories s = print $ sum $ take 3 (sortBy (comparing Down) (elvesTotalCalories s))
-
-part2 = withFile' "day1.txt" top3Calories
 
 split :: [String] -> [[String]]
 split s = case dropWhile (== "") s of
     [] -> []
     s' -> w : split s''
         where (w, s'') = break (== "") s'
+
+mapRead :: [[String]] -> [[Int]]
+mapRead s =  (map . map) (read :: String -> Int) s
+
+-- Day 1 --
+elvesTotalCalories :: String -> [Int]
+elvesTotalCalories s = map (sum) . mapRead . split $ lines s
+
+maxCalories :: String -> Int
+maxCalories s = maximum $ elvesTotalCalories s
+
+part1 = withFile' "day1.txt" maxCalories
+
+top3Calories :: String -> Int
+top3Calories s = (sum . take 3 . sortBy (comparing Down)) $ elvesTotalCalories s
+
+part2 = withFile' "day1.txt" top3Calories
+
 
 -- Day 2 --
 
